8f53fb14f0baf493eaf53c0288a84428d1130ef2
anonto hossain <anonto@users.noreply.github.com>
2021-05-17 15:59:16 +0600

    RobotMain.py

---
+++ RobotMain.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import KeyPressModule as kp
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+pwm=GPIO.PWM(11,50)
+
+
+
+
+kp.init()
+
+def main()
+
+  if kp.getKey('UP'):
+      ####################################
+  elif kp.getKey('DOWN'):
+      ####################################
+  elif kp.getKey('LEFT'):
+      pwm.ChangeDutyCycle(2)
+  elif kp.getKey('RIGHT'):
+      pwm.ChangeDutyCycle(12)
+
+  else:
+      pwm.stop()
+      GPIO.cleanup()
